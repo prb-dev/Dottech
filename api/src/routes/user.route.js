750e3bb9c706681631fded092b8c0c71150dccd8
prb-dev <prabashwara.in@gmail.com>
2025-03-05 17:41:10 +0530

    Create the get / delete students feature

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,10 +1,20 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import {
+  deleteStudent,
+  getStudents,
+  updateUser,
+} from "../controllers/user.controller.js";
 import { verifyToken } from "../middlewares/verifyToken.middleware.js";
 import { verifyUser } from "../middlewares/verifyUser.middleware.js";
+import { getMarks, updateMarks } from "../controllers/marks.controller.js";
+import { verifyTeacher } from "../middlewares/verifyTeacher.middleware.js";
 
 const router = express.Router();
 
-router.put("/:id", verifyToken, verifyUser, updateUser);
+router.get("/", verifyToken, verifyTeacher, getStudents);
+router.get("/:id/marks", verifyToken, verifyUser, getMarks);
+router.put("/:id", verifyToken, updateUser);
+router.patch("/:id/marks/:mid", verifyToken, verifyUser, updateMarks);
+router.delete("/:id", verifyToken, verifyTeacher, deleteStudent);
 
 export default router;
--- api/src/utils/validator.util.js
+++ api/src/utils/validator.util.js
@@ -1,11 +1,22 @@
-import { signSchema, signupSchema } from "./validationSchemas/auth.schema.js";
-import { updateUserSchema } from "./validationSchemas/user.schema.js";
+import { signinSchema, signupSchema } from "./validationSchemas/auth.schema.js";
+import {
+  getMarksSchema,
+  updateMarksSchema,
+} from "./validationSchemas/marks.schema.js";
+import {
+  deleteStudentSchema,
+  updateUserSchema,
+} from "./validationSchemas/user.schema.js";
 
 const validate = (schema) => (payload) => {
   return schema.validate(payload, { abortEarly: false });
 };
 
 export const validateSignup = validate(signupSchema);
-export const validateSignin = validate(signSchema);
+export const validateSignin = validate(signinSchema);
 
 export const validateUpdateUser = validate(updateUserSchema);
+export const validateDeleteStudent = validate(deleteStudentSchema);
+
+export const validateGetMarks = validate(getMarksSchema);
+export const validateUpdateMarks = validate(updateMarksSchema);
